a2707c8146c4d14fa32c155e18e51550bb232d9f
Siwoo Jeon <me@migan.co.kr>
2025-04-19 22:08:20 +0900

    fix: Adding page in search method

--- examples/bot/search/search.go
+++ examples/bot/search/search.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	k := krlistsgo.New("", "") // 필요 없는 값
-	data, err := k.SearchBots("관리")
+	data, err := k.SearchBots("관리", 1)
 	if err != nil {
 		panic(err)
 	}
--- search.go
+++ search.go
@@ -7,8 +7,8 @@ import (
 	"net/url"
 )
 
-// SearchResult는 검색한 결과 값입니다.
-type SearchResult[T Server[User[string, string]] | Bot[string]] struct {
+// PageResult는 검색하거나 하트 랭킹의 결과 값입니다.
+type PageResult[T Server[User[string, string]] | Bot[string]] struct {
 	Data       []T          `json:"Data"`
 	Current    int          `json:"currentPage"`
 	Total      int          `json:"totalPage"`
@@ -17,7 +17,7 @@ type SearchResult[T Server[User[string, string]] | Bot[string]] struct {
 	query      string       `json:"-"`
 }
 
-func search[T Server[User[string, string]] | Bot[string]](c *http.Client, searchType string, query string, page int) (result *SearchResult[T], err error) {
+func search[T Server[User[string, string]] | Bot[string]](c *http.Client, searchType string, query string, page int) (result *PageResult[T], err error) {
 	query = url.QueryEscape(query)
 	resp, err := get(c, fmt.Sprintf("/search/%s?query=%s&page=%d", searchType, query, page), []map[string]string{})
 	if err != nil {
@@ -32,17 +32,23 @@ func search[T Server[User[string, string]] | Bot[string]](c *http.Client, search
 }
 
 // SearchBots는 해당 검색어로 봇을 검색합니다.
-func (k *KrLists) SearchBots(query string) (*SearchResult[Bot[string]], error) {
-	return search[Bot[string]](k.Client, "bots", query, 1)
+func (k *KrLists) SearchBots(query string, page int) (*PageResult[Bot[string]], error) {
+	if page <= 0 {
+		return nil, SearchPositiveNumberErr
+	}
+	return search[Bot[string]](k.Client, "bots", query, page)
 }
 
 // SearchServers는 해당 검색어로 서버를 검색합니다.
-func (k *KrLists) SearchServers(query string) (*SearchResult[Server[User[string, string]]], error) {
-	return search[Server[User[string, string]]](k.Client, "servers", query, 1)
+func (k *KrLists) SearchServers(query string, page int) (*PageResult[Server[User[string, string]]], error) {
+	if page <= 0 {
+		return nil, SearchPositiveNumberErr
+	}
+	return search[Server[User[string, string]]](k.Client, "servers", query, page)
 }
 
 // Next는 다음 페이지로 넘깁니다.
-func (s *SearchResult[T]) Next() (*SearchResult[T], error) {
+func (s *PageResult[T]) Next() (*PageResult[T], error) {
 	if s.Current >= s.Total {
 		return nil, SearchLastPageErr
 	}
@@ -50,7 +56,7 @@ func (s *SearchResult[T]) Next() (*SearchResult[T], error) {
 }
 
 // Previous는 이전 페이지로 넘깁니다.
-func (s *SearchResult[T]) Previous() (*SearchResult[T], error) {
+func (s *PageResult[T]) Previous() (*PageResult[T], error) {
 	if s.Current <= 1 {
 		return nil, SearchFirstPageErr
 	}
@@ -58,8 +64,8 @@ func (s *SearchResult[T]) Previous() (*SearchResult[T], error) {
 }
 
 // Select는 특정 페이지로 바로 이동합니다.
-func (s *SearchResult[T]) Select(page int) (*SearchResult[T], error) {
-	if page < 0 {
+func (s *PageResult[T]) Select(page int) (*PageResult[T], error) {
+	if page <= 0 {
 		return nil, SearchPositiveNumberErr
 	}
 

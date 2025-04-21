6ae95fcac48885319bced64296b81cf9d63d38aa
Siwoo Jeon <me@migan.co.kr>
2025-04-21 22:12:38 +0900

    fix: Adding argument token in CheckVote method

--- vote.go
+++ vote.go
@@ -39,8 +39,9 @@ func (k *KrLists) CheckBotVote(userID string) (*CheckVote, error) {
 }
 
 // CheckVote는 userID가 해당 봇에 투표했는지를 확인합니다.
-func (b *Bot[T]) CheckVote(userID string) (*CheckVote, error) {
-	return checkVote(b.client, b.identify, "bots", userID)
+// token 인수는 해당 봇이 당신의 봇인지를 모르기 때문에 필요합니다.
+func (b *Bot[T]) CheckVote(token, userID string) (*CheckVote, error) {
+	return checkVote(b.client, &Identify{b.ID, token}, "bots", userID)
 }
 
 // CheckServerVote는 userID가 당신의 서버에 투표했는지를 확인합니다.
@@ -49,6 +50,7 @@ func (k *KrLists) CheckServerVote(userID string) (*CheckVote, error) {
 }
 
 // CheckVote는 userID가 해당 서버에 투표했는지를 확인합니다.
-func (s *Server[T]) CheckVote(userID string) (*CheckVote, error) {
-	return checkVote(s.client, s.identify, "servers", userID)
+// token 인수는 해당 봇이 당신의 서버인지를 모르기 때문에 필요합니다.
+func (s *Server[T]) CheckVote(token, userID string) (*CheckVote, error) {
+	return checkVote(s.client, &Identify{s.ID, token}, "servers", userID)
 }
